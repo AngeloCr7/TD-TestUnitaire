5bc0e169c4c7652692c91dd55402191670a1134c
Angelo <brasiliafan@gmail.com>
2024-04-02 10:57:41 +0200

    Ajout de methode de test d'egalite

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -8,11 +8,13 @@ class MoneyTest {
 
 	@Test
 	public void testSimpleAdd() {
-		Money m12CHF = new Money(12,"CHF");//création des données
+		Money m12CHF = new Money(12,"CHF");
 		Money m14CHF = new Money(14,"CHF");
-		Money expected = new Money(26,"CHF");
-		Money result = m12CHF.add(m14CHF);//éxécutuion de la methode testée
-		assertTrue(expected.equals(result));//comparaison
+		
+		assertTrue(!m12CHF.equals(null));
+		assertEquals(m12CHF,m12CHF);
+		assertEquals(m12CHF,new Money(12,"CHF"));
+		assertTrue(!m12CHF.equals(m14CHF));
 	}
 
 }
