d90e4b3295b4552120d4cd57d5b774e9e9d7b300
Angelo <brasiliafan@gmail.com>
2024-04-02 10:32:23 +0200

    ajout de la methode de Test de la classe Money

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 class MoneyTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testSimpleAdd() {
+		Money m12CHF = new Money(12,"CHF");//création des données
+		Money m14CHF = new Money(14,"CHF");
+		Money expected = new Money(26,"CHF");
+		Money result = m12CHF.add(m14CHF);//éxécutuion de la methode testée
+		assertTrue(expected.equals(result));//comparaison
 	}
 
 }
